c8ff84b3ad1da819285a110565564ace2366db90
jean carlos <31245828+alvarez37@users.noreply.github.com>
2019-05-24 13:10:12 -0500

    pc
    esto no esta bien

--- lista enlazada/main.cpp
+++ lista enlazada/main.cpp
@@ -22,16 +22,19 @@ class lista_enlazada{
         bool find(T valor,nodo <T> * &ptr);
         void add (T dato);
         bool buscar (T dato);
+        int contador=0;
 
         void show();
         void remover(T dato);
 
 };
 
+
 template <class T>
 bool lista_enlazada<T>::find(T valor,nodo <T> * &ptr){
     ptr=NULL;
-    for (nodo<T> *p=head; p and p->valor<=valor ; ptr=p , p=p->next) {
+    int i=0;
+    for ( nodo<T> *p=head; p and p->valor<=valor and i<contador ; ptr=p , p=p->next,i++) {
       if (p->valor==valor) {
 
         return true;
@@ -59,9 +62,13 @@ void lista_enlazada<T>::add(T dato){
 
   if (!find(dato,pos)) {
     if (!pos) {
+      contador++;
       head= new nodo<T>(dato,head);
+      head->next=head;
+      std::cout << "veces  " << '\n';
       }
     else{
+      contador++;
       pos->next=new nodo<T>(dato,pos->next);
       }
     }
@@ -71,8 +78,11 @@ template <class T>
 void lista_enlazada<T>::show() {
 
   std::cout  << '\n';
-  for (nodo <T> *p=head; p ; p=p->next) {
-      std::cout << "[ id: " << p<< " valor: " << p->valor << " ]"<<" ";
+  // for (nodo <T> *p=head; p ; p=p->next) {
+  int i=0;
+  for (nodo <T> *p=head ; p && i<contador ; p=p->next, i++) {
+
+      std::cout << i<< "     [ id: " << p<< " valor: " << p->valor << " apunta  " << p->next<<  " ]"<<" "<< endl;
       // std::cout <<  p->valor <<'\t';
 
     }
@@ -88,11 +98,14 @@ void lista_enlazada<T>::remover(T dato){
       if (find(dato,pos)) {
         find(dato,pos);
         if(pos!=0){
+          contador--;
           pos->next=pos->next->next;
           delete pos->next;
         }
 
         else{
+
+          contador--;
           head=head->next;
           delete head;
         }
@@ -108,18 +121,25 @@ void lista_enlazada<T>::remover(T dato){
 int main(){
 
     lista_enlazada <int> p1;
-        p1.add(1);
-        p1.add(10);
+
+        p1.show();
         p1.add(100);
+        p1.show();
         p1.add(1000);
-
+        p1.add(1);
         p1.show();
-        p1.remover(10);
-        p1.show();
-        std::cout << p1.buscar(1) << '\n';
-
+        p1.add(10);
         p1.show();
 
+        // p1.remover(10);
+        // p1.add(101);
+        p1.add(1001);
+        p1.show();
+
+
+        std::cout << p1.contador << '\n';
+
+        p1.show();
 
     return 0;
 }
