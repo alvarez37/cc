3dd45c40657ce57b2e2015b34405b9e568264e24
jean carlos <31245828+alvarez37@users.noreply.github.com>
2019-05-23 22:56:39 -0500

    pc
    ya muestra datos

--- lista enlazada/main.cpp
+++ lista enlazada/main.cpp
@@ -18,41 +18,61 @@ template <class T>
 class lista_enlazada{
 
     public:
-        nodo<T> head=NULL;
+        int tamanio=0;
+        nodo<T> *head=NULL;
         bool find(T valor,nodo <T> * &ptr);
         void add (T dato);
+        void show();
 
 };
 
 template <class T>
 bool lista_enlazada<T>::find(T valor,nodo <T> * &ptr){
     ptr=NULL;
-    for (nodo<T> *p=head; p and p->valor<=valor , ptr=p; p=p->next) {
+    for (nodo<T> *p=head; p and p->valor<=valor ; ptr=p , p=p->next) {
       if (p->valor==valor) {
         return true;
-      }
+         }
+       }
       return false;
-    }
 }
 
 template <class T>
 void lista_enlazada<T>::add(T dato){
   nodo<T> *pos;
+
   if (!find(dato,pos)) {
     if (!pos) {
+      tamanio++;
       head= new nodo<T>(dato,head);
       }
     else{
+      tamanio++;
       pos->next=new nodo<T>(dato,pos->next);
       }
     }
   }
 
+template <class T>
+void lista_enlazada<T>::show() {
+  nodo<T> *p=head;
+  for (int i=0; p and i <=  tamanio ; p=p->next) {
+      std::cout << p->valor << '\n';
+    }
+}
 
-int main()
-{
-    lista_enlazada<int> p1();
-    p1.add(<int> 5);
+int main(){
+
+    // nodo<int> a(4);
+    // nodo<int> *b=&a;
+    // nodo<int> c(23,b);
+    //
+    // std::cout << b->valor << '\n';
+    lista_enlazada <int> p1;
+    p1.add(3);
+    p1.add(23);
+    p1.add(13);
+    p1.show();
 
     return 0;
 }
